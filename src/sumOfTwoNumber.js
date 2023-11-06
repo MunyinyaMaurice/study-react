4ba904037a93daffc2a6092d1f621d068e32791d
MunyinyaMaurice <munyinya13@gmail.com>
2023-11-06 11:23:58 +0200

    creating function to substract in two  numbers

--- src/sumOfTwoNumber.js
+++ src/sumOfTwoNumber.js
@@ -1,3 +1,10 @@
 export function add(num1 , num2){
 return num1+num2
 }
+export function sub(num1 , num2){
+    return num1 - num2
+    }
+
+export default function abc(){
+        return 100;
+        }
